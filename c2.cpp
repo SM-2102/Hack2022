fe0ac59daa000be0842edf49d968ed7228d46e46
dipandutta19 <116013718+dipandutta19@users.noreply.github.com>
2022-10-17 20:47:02 +0530

    Add files via upload

---
+++ c2.cpp
@@ -0,0 +1,18 @@
+//write a c program to find a maximum between two numbers//
+# include <stdio.h>
+# include <conio.h>
+main()
+{
+	int a,b;
+	printf ("enter two numbers ");
+	scanf ("%d %d", &a,&b);
+	if (a>b)
+	{
+	printf ("a is a maximum number");
+	}
+	else
+	{
+	printf ("b is a maximum number");
+    }
+}
+ 
